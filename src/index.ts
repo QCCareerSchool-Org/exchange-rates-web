c940ac18b2e6ad0efe6a2646d44ae3e80c760d8a
Dave Welsh <dave@qccareerschool.com>
2024-10-01 11:11:15 -0400

    Removed request-promise

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import Debug from 'debug';
 import dotenv from 'dotenv';
 import { promises as fs } from 'fs';
 import mysql from 'promise-mysql';
-import rp from 'request-promise';
 
 const debug = Debug('index');
 
@@ -10,42 +9,52 @@ dotenv.config();
 
 debug('Starting request');
 
+const currencyCodes = [ 'USD', 'AUD', 'CAD', 'NZD', 'GBP', 'ZAR' ];
+
+const getRates = async (): Promise<Record<string, number>> => {
+  const url = `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}&symbols=${currencyCodes.join(',')}}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+
+  const body: IExchangeResult = await response.json();
+
+  if (typeof body.rates === 'undefined') {
+    throw new Error('No rates supplied');
+  }
+
+  if (typeof body.date !== 'string') {
+    throw new Error('No date supplied');
+  }
+
+  if (!/^\d{4}-\d{2}-\d{2}$/u.test(body.date)) {
+    throw new Error('Unrecognized date format');
+  }
+
+  if (typeof body.rates.USD === 'undefined') {
+    throw new Error('No USD rate found');
+  }
+
+  // convert the rates to a USD base
+  const rates: Record<string, number> = {};
+  rates.USD = 1;
+  rates.EUR = 1 / body.rates.USD;
+  for (const r in body.rates) {
+    if (!body.rates.hasOwnProperty(r)) continue;
+    if (r === 'USD' || r === 'EUR') continue;
+    rates[r] = body.rates[r] * rates.EUR;
+  }
+
+  return rates;
+}
+
 (async () => {
 
   try {
-
-    // perform a GET request for the currency data
-    const url = `http://data.fixer.io/api/latest?access_key=${process.env.FIXER_API_KEY}&symbols=USD,AUD,CAD,NZD,GBP,ZAR`;
-    const response: IExchangeResult = await rp(url, { json: true });
-console.log(response);
-    debug(response);
-
-    // see if the data makes sense
-    if (typeof response.rates === 'undefined') {
-      throw new Error('No rates supplied');
-    }
-
-    if (typeof response.date !== 'string') {
-      throw new Error('No date supplied');
-    }
-
-    if (!/^\d{4}-\d{2}-\d{2}$/u.test(response.date)) {
-      throw new Error('Unrecognized date format');
-    }
-
-    if (typeof response.rates.USD === 'undefined') {
-      throw new Error('No USD rate found');
-    }
-
-    // convert the rates to a USD base
-    const rates: any = {};
-    rates.USD = 1;
-    rates.EUR = 1 / response.rates.USD;
-    for (const r in response.rates) {
-      if (!response.rates.hasOwnProperty(r)) continue;
-      if (r === 'USD' || r === 'EUR') continue;
-      rates[r] = response.rates[r] * rates.EUR;
-    }
+    const rates = await getRates();
+    console.log(rates);
 
     // set up the database configuration options
     const options: mysql.ConnectionConfig = {
@@ -69,24 +78,15 @@ console.log(response);
     }
 
     try {
-
       const connection = await mysql.createConnection(options);
 
       try {
-
         // find out which currencies need updating
-        let sqlSelect;
-        if (typeof process.env.ALL_CURRENCIES !== 'undefined' && process.env.ALL_CURRENCIES === 'TRUE') {
-          sqlSelect = "SELECT code FROM currencies WHERE NOT code = 'USD'";
-        } else {
-          sqlSelect = "SELECT code FROM currencies WHERE NOT code = 'USD' AND NOT `update` = 0";
-        }
+        const sqlSelect = "SELECT code FROM currencies WHERE NOT code = 'USD' AND NOT `update` = 0";
 
         const currencies: ICurrency[] = await connection.query(sqlSelect);
 
         const sqlUpdate = 'UPDATE currencies SET exchange = ?, last_updated = NOW() WHERE code = ?';
-
-        const promises: Array<PromiseLike<any>> = [];
 
         // update the currencies in parallel
         for (const currency of currencies) {
@@ -97,12 +97,9 @@ console.log(response);
           const rate = rates[currency.code];
           debug(`Updating ${currency.code}: ${rate}`);
           if (typeof process.env.TESTING === 'undefined') {
-            promises.push(connection.query(sqlUpdate, [ rate, currency.code ]));
+            await connection.query(sqlUpdate, [rate, currency.code]);
           }
         }
-
-        // wait for all of the updates
-        await Promise.all(promises);
 
       } finally {
         connection.end();
@@ -113,7 +110,6 @@ console.log(response);
   } catch (err) {
     debug('Error: ' + (err instanceof Error ? err.message : err));
   }
-
 })();
 
 interface IExchangeResult {
